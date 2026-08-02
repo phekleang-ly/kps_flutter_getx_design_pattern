e0176b9250cb4061776ce83d072b2e9f14a25ca9
phekleang-ly <lyphekleang@gmail.com>
2026-08-02 09:17:46 +0700

    last

--- lib/app/cores/constants/constant_uri.dart
+++ lib/app/cores/constants/constant_uri.dart
@@ -1,5 +1,5 @@
 class ConstantUri {
-  static const String baseUrl = "http://192.168.72.78:30033";
+  static const String baseUrl = "http://192.168.73.179:30033";
   static const String loginPath = "$baseUrl/api/oauth/token";
   static const String refreshPath = "$baseUrl/api/oauth/refresh";
 
--- lib/app/cores/network/api_network_service_impl.dart
+++ lib/app/cores/network/api_network_service_impl.dart
@@ -5,23 +5,28 @@ import 'package:kps_flutter_getx_design_battern/app/cores/models/auth/LoginReque
 import 'package:kps_flutter_getx_design_battern/app/cores/models/auth/LoginResponse.dart';
 import 'package:kps_flutter_getx_design_battern/app/cores/network/api_network_service.dart';
 import 'package:http/http.dart' as httpClient;
+
 class ApiNetworkServiceImpl extends ApiNetworkService {
+  var headers = {"Content-Type": "application/json"};
   @override
   Future<LoginResponse> login(LoginRequest req) async {
     LoginResponse loginResponse = LoginResponse();
     //URL
     var url = Uri.parse(ConstantUri.loginPath);
     //Call To API
-    var response = await httpClient.post(url, body: jsonEncode(req.toJson()));
+    var response = await httpClient.post(
+      url,
+      body: jsonEncode(req.toJson()),
+      headers: headers,
+    );
     //Check Condition status code
-    if(response.statusCode == 401){
+    if (response.statusCode == 401) {
       return loginResponse;
     }
     //Mapping Response
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       loginResponse = LoginResponse.fromJson(jsonDecode(response.body));
     }
     return loginResponse;
   }
-
 }
